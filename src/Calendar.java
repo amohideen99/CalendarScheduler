2ef298adda3142788880b6e28348b70c7eb1db16
amohideen99 <amohideen99@gmail.com>
2018-02-27 07:47:49 -0800

    Fix BoxLayout on outer Jpanel

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,23 +1,32 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
 
 public class Calendar {
 
+
+
     JFrame frame;
-    JPanel outer;
+    JPanel inner;
     JPanel panel;
+    JPanel outer;
     JLabel month;
+    JButton nextMonth;
+    LocalDate localDate;
 
 
     public Calendar() {
 
         frame = new JFrame("Calendar");
 
-        outer = new JPanel();
-        outer.setLayout(new BoxLayout(outer, BoxLayout.Y_AXIS));
+        outer = new JPanel(new BoxLayout(outer, BoxLayout.PAGE_AXIS));
+
+        inner = new JPanel();
+        inner.setLayout(new BorderLayout());
 
         panel = new JPanel();
 
@@ -27,7 +36,7 @@ public class Calendar {
 
 
         Date date = new Date();
-        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
 
         initMonth(localDate);
 
@@ -35,10 +44,26 @@ public class Calendar {
         month.setFont(new Font(Font.SERIF, Font.BOLD, 30));
         month.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        outer.add(month);
+
+
+        nextMonth = new JButton();
+        nextMonth.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                LocalDate newDate = localDate.plusMonths(1);
+                initMonth(newDate);
+                localDate = newDate;
+            }
+        });
+        inner.add(month, BorderLayout.PAGE_START);
+        inner.add(nextMonth, BorderLayout.AFTER_LINE_ENDS);
+
+        outer.add(inner);
         outer.add(panel);
 
         frame.add(outer);
+     //   frame.add(panel);
         frame.setMinimumSize(new Dimension(800, 500));
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
@@ -46,11 +71,18 @@ public class Calendar {
 
     public void initMonth(LocalDate date) {
 
+        panel.removeAll();
+
         for (int i = 1; i < date.lengthOfMonth() + 1; i++) {
 
             panel.add(new Day(date, i));
         }
+
+        panel.revalidate();
+        panel.repaint();
     }
+
+
 
     public static void main(String[] args) {
 
